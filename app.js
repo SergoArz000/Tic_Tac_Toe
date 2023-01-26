8270dab4c4ef433a9cf65efaa517d5f3771976e4
Sergo <sergo.arzumanyan000@gmail.com>
2023-01-26 12:01:52 +0400

    added win/lose/tie functionality

--- app.js
+++ app.js
@@ -1,31 +1,78 @@
-let buttonsContainer = document.querySelector(".buttonsContainer");
+const buttonsContainer = document.querySelector(".buttonsContainer");
+let buttons = document.querySelectorAll(".button");
+let winCount = 0;
 
-
+// 										FUNCTIONS FOR BREVITY
 let randomNum = ( length ) => {
 	return Math.round( Math.random() * length );
 }
 
-let autoPicker = () => {
-	let buttonNodes = document.querySelectorAll(".empty");
-	let buttons = Array.from( buttonNodes );
-	let num = randomNum( buttons.length - 1 );
-	if ( buttons.length <= 1 ) {
-		return;
+let buttonsCheck = () => {
+	if ( 
+		buttons[0].innerHTML && buttons[1].innerHTML && buttons[2].innerHTML &&
+		buttons[3].innerHTML && buttons[4].innerHTML && buttons[5].innerHTML &&
+		buttons[6].innerHTML && buttons[7].innerHTML && buttons[8].innerHTML
+		) {
+		return true;
 	}
+}
 
-	if ( buttons[num].innerHTML === ""  ) {
-		buttons[num].innerHTML = "O";
-		buttons[num].classList.add("O");
-		buttons[num].classList.remove("empty");
+let winCheck = ( char ) => {
+	if ( 
+		buttons[0].innerHTML === char && buttons[4].innerHTML === char && buttons[8].innerHTML === char  
+		||
+		buttons[2].innerHTML === char && buttons[4].innerHTML === char && buttons[6].innerHTML === char  		
+		||
+		buttons[0].innerHTML === char && buttons[1].innerHTML === char && buttons[2].innerHTML === char  
+		||
+	    buttons[3].innerHTML === char && buttons[4].innerHTML === char && buttons[5].innerHTML === char  
+	    ||
+	    buttons[6].innerHTML === char && buttons[7].innerHTML === char && buttons[8].innerHTML === char
+	    ||
+	    buttons[0].innerHTML === char && buttons[3].innerHTML === char && buttons[6].innerHTML === char  
+	    ||  
+	    buttons[1].innerHTML === char && buttons[4].innerHTML === char && buttons[7].innerHTML === char  
+	    ||
+	    buttons[2].innerHTML === char && buttons[5].innerHTML === char && buttons[8].innerHTML === char  
+		) {
+		winCount++
+		if ( winCount === 1 ) {
+			console.log(`${char} =>  wins `);	
+			buttonsContainer.removeEventListener( "click" , handler )
+		}
+	} else if ( buttonsCheck() ) { 
+		console.log("nichya");
 	}
 }
 
 
-buttonsContainer.addEventListener( "click"  , ( evn ) => {
+
+let autoPicker = () => {
+	let buttonNodes = document.querySelectorAll(".empty");
+	let buttonsArr = Array.from( buttonNodes );
+	let num = randomNum( buttonsArr.length - 1 );
+	if ( buttonsArr.length <= 1 ) {
+		return;
+	}
+
+	if ( buttonsArr[num].innerHTML === ""  ) {
+		buttonsArr[num].innerHTML = "O";
+		buttonsArr[num].classList.add("O");
+		buttonsArr[num].classList.remove("empty");
+		winCheck( "O" );
+	}
+}
+
+//										ADDING EVENT 
+function handler ( evn ) {
 	if ( !evn.target.innerHTML ) {
 		evn.target.innerHTML = "X";
 		evn.target.classList.remove("empty");
 		evn.target.classList.add("X");
+		winCheck( "X" );
 		autoPicker();
 	}
-});
+}
+
+buttonsContainer.addEventListener( "click"  , handler );
+
